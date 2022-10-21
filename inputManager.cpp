ccbad5893f5913441ab0df648f699bde3544683a
Joey Schutz <schutzjoey@gmail.com>
2022-10-21 13:23:41 -0400

    first pass

---
+++ inputManager.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+
+const std::string OUTPUT_FILE_PATH = "Assets/CPP_Applications/Windows/_communications/input.txt";
+
+std::string getUserInput();
+void writeToFile(std::string str);
+
+
+int main()
+{
+    system("cd");
+    
+    while(true) {
+        std::string input = getUserInput();
+        if(input == "exit") {
+            break;
+        }
+        writeToFile(input);
+    }
+
+    return 0;
+}
+
+
+
+std::string getUserInput()
+{
+    std::string line;
+    std::getline(std::cin, line);
+    return line;
+}
+
+
+void writeToFile(std::string str)
+{
+    std::ofstream myfile;
+    myfile.open (OUTPUT_FILE_PATH);
+    myfile << str << std::endl;
+    myfile.close();
+}
